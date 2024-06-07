2b9435ea9b6486661672a72a8818a95369dfbc85
Ben Klinovsky <bnk989@gmail.com>
2024-06-07 18:22:49 +0300

    Add api to create new car listing

---
+++ server/api/car/listings/index.js
@@ -0,0 +1,52 @@
+// import { PrismaClient } from '@prisma/client/extension'
+import Joi from 'joi'
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+const schema = Joi.object({
+    make: Joi.string().required(),
+    model: Joi.string().required(),
+    year: Joi.number()
+        .integer()
+        .min(1886)
+        .max(new Date().getFullYear() + 1),
+    miles: Joi.number().integer().min(0).required(),
+    city: Joi.string().required(),
+    numberOfSeats: Joi.number().integer().min(0).max(100).required(),
+    features: Joi.array().items(Joi.string()).required(),
+    image: Joi.string().required(),
+    ListerId: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().min(0).max(1000000),
+    name: Joi.string().required(),
+}) //.required()
+
+export default defineEventHandler(async (e) => {
+    const body = await readBody(e)
+
+    const { error, value } = schema.validate(body)
+
+    if (error) {
+        throw createError({ statusCode: 412, statusMessage: error.message })
+    }
+
+    const car = await prisma.car.create({
+        data: {
+            make: value.make.toLowerCase(),
+            model: value.model.toLowerCase(),
+            year: +value.year,
+            miles: +value.miles,
+            city: value.city.toLowerCase(),
+            numberOfSeats: value.numberOfSeats,
+            features: value.features,
+            description: value.description,
+            image: value.image,
+            ListerId: value.ListerId,
+            price: +value.price,
+            name: value.name.toLowerCase(),
+        },
+    })
+
+    return car
+})
